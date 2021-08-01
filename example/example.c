908356ffbb06fe937e4eaa6c1859cc57e5b76887
hirameunagi <hirameunagi@gmail.com>
2021-08-01 17:25:50 +0900

    example2

--- example/example.c
+++ example/example.c
@@ -8,8 +8,7 @@ void exampe1()
 
   *ll_push(list) = 10;
   *ll_push(list) = 20;
-  // *ll_insert_after(ll_first(list)) = 20;
-  // *ll_push(list) = 30;
+  *ll_push(list) = 30;
 
   int *value = ll_first(list);
   while (value != NULL)
@@ -21,7 +20,41 @@ void exampe1()
   ll_free(list);
 }
 
+typedef struct
+{
+  int x;
+  int y;
+} Point;
+
+void example2()
+{
+  Point *list;
+  list = ll_new(list);
+
+  Point *node = ll_push(list);
+  node->x = 10;
+  node->y = -10;
+
+  node = ll_insert_before(node);
+  node->x = 20;
+  node->y = -20;
+
+  node = ll_insert_before(node);
+  node->x = 30;
+  node->y = -30;
+
+  node = ll_first(list);
+  while (node != NULL)
+  {
+    printf("[%d %d]\n", node->x, node->y);
+    node = ll_next(node);
+  }
+
+  ll_free(list);
+}
+
 void main()
 {
   exampe1();
+  example2();
 }
